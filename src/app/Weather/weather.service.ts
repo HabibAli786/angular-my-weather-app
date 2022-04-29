f1137339be434d43a2a32099378f1e8932f0997c
Habib Ali <Habib.Ali@telent.com>
2022-04-29 15:29:07 +0100

    add api

---
+++ src/app/Weather/weather.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, throwError } from "rxjs";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WeatherService {
+    // API Key f6db6883d8684c6c931100901213103
+
+    apiKey = 'f6db6883d8684c6c931100901213103'
+    city = 'London'
+    
+  
+    weather$ = this.http.get(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}&aqi=no`)
+    .pipe(
+        tap(data => console.log('Data returned: ', data)),
+        catchError(this.handleError)
+    );
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getCityWeather(city: string) {
+      console.log(city)
+    }
+
+    private handleError(err: HttpErrorResponse): Observable<never> {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage: string;
+        if (err.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          errorMessage = `Backend returned code ${err.status}: ${err.message}`;
+        }
+        console.error(err);
+        return throwError(() => errorMessage);
+      }
+}
